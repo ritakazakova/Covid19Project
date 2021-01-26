401bbb43b5e161641dff8f5925af5ab1115e8df5
Rita <mkazakova00@mail.ru>
2021-01-26 21:31:39 +0300

    Tests for value on Usernames and Passwords texstFields

---
+++ Covid19ProgectTests/Covid19ProgectTests.swift
@@ -0,0 +1,40 @@
+//
+//  Covid19ProgectTests.swift
+//  Covid19ProgectTests
+//
+//  Created by Rita Kazakova on 1/26/21.
+//
+
+import XCTest
+
+@testable import Covid19Progect
+
+class TestTextInFeilds: XCTestCase {
+    var validator: CheckLoginPassword!
+    
+    override func setUpWithError() throws {
+        let validator = CheckLoginPassword.init()
+        self.validator = validator
+    }
+
+    func testThatOnFilledCheckNoPassed() throws {
+
+        let result = validator.checkLoginAndPassword(userName: "", password: "")
+        XCTAssertFalse(result)
+    }
+
+    func testThatOnFilledCheckPassed() throws {
+        let result = validator.checkLoginAndPassword(userName: "123", password: "123")
+        XCTAssertTrue(result)
+    }
+    
+    func testThatOnPasswordTextOnUsernameNoText() throws {
+        let result = validator.checkLoginAndPassword(userName: "", password: "123")
+        XCTAssertFalse(result)
+    }
+    
+    func testThatOnPasswordNoTextOnUsernameText() throws {
+        let result = validator.checkLoginAndPassword(userName: "123", password: "")
+        XCTAssertFalse(result)
+    }
+}
